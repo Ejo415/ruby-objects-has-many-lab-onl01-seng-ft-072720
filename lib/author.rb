b59331b4667d472ca71b72ebcf073379ae62da73
Eugene O'Neil <ejo7220@gmail.com>
2020-08-10 18:52:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,13 +19,12 @@ class Author
     @@post_count += 1
   end
 
-  def add_post_by_title(name)
+  def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post 
+    @posts << post
     post.author = self
     @@post_count += 1
   end
-
   def self.song_count
     @@song_count
   end
